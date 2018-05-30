310f4107dbbab60a1d30020bb0d9f609248f9f18
Dmitry <van777dim@gmail.com>
2018-05-31 01:32:55 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i=100;
+        do {
+            System.out.println("Element i is: "+i);
+        i+=2;
+        } while (i<110);
+//	for (int i = 0; i<12; i++)
+//	    System.out.println("Element i is: "+i);
+	}
+}
